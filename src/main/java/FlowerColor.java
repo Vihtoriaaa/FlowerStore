a3dfebea0840e27076f3bbf2a52905895653c973
Viktoria Maksymiuk <vikamaksymiuk24@gmail.com>
2021-10-31 03:20:45 +0200

    commit

--- src/main/java/FlowerColor.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public enum FlowerColor {
-    YELLOW, RED, BLUE, BLACK, WHITE;
-    ArrayList<FlowerColor> flower_colors = new ArrayList<>();
-    public static FlowerColor getRandomColor() {
-        return values()[(int) (Math.random() * values().length)];
-    }
-}
