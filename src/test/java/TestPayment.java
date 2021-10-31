fb7a7a752aad708723fa64784d3eef479202c15a
Viktoria Maksymiuk <vikamaksymiuk24@gmail.com>
2021-10-31 03:29:44 +0200

    test committed

--- src/test/java/TestPayment.java
+++ src/test/java/TestPayment.java
@@ -1,19 +1,27 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import payment.*;
+import org.junit.jupiter.api.BeforeEach;
+
 
 public class TestPayment {
+    private PayPalPaymentStrategy paypal;
+    private CreditCardPaymentStrategy credit;
+
+    @BeforeEach
+    void setUp() {
+        paypal = new PayPalPaymentStrategy();
+        credit = new CreditCardPaymentStrategy();
+    }
 
     @Test
     void TestPayPal() {
-        PayPalPaymentStrategy paypal = new PayPalPaymentStrategy();
         assertEquals(true, paypal.pay(1000.5));
         assertEquals(true, paypal.pay(0.5));
     }
 
     @Test
     void TestCreditCart() {
-        CreditCardPaymentStrategy credit = new CreditCardPaymentStrategy();
         assertEquals(true, credit.pay(5436.763234));
         assertEquals(true, credit.pay(0.00));
     }
